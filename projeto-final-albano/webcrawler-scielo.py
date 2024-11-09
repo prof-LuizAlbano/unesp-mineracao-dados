54d829d3bcb2936dba12e392b6953217770aa0d5
Luiz Albano <contato@luizalbano.com.br>
2024-11-09 14:59:49 -0300

    Refatoração das funções para tratamento da URL da página de busca e de carregamento das páginas

--- projeto-final-albano/webcrawler-scielo.py
+++ projeto-final-albano/webcrawler-scielo.py
@@ -9,7 +9,7 @@ Config vars
 """
 
 # Amount of pages
-pages_total = 130
+pages_total = 1 #130
 
 # Amount of items (papers)
 pages_items = 50
@@ -29,6 +29,17 @@ def database_connect(database_file):
         print("Failed to open datanase: ", database_file, e)
 
 
+"""
+Configura a URL de busca na plataforma SciELO.
+
+:param page: Número da página a ser carregada
+:param count: Quantidade de itens carregados
+:return: retorna a URL formatada
+"""
+def parse_url_scielo(page=0, count=50):
+    from_count = 0 if page == 1 else (page * count) - 49
+    return f"https://search.scielo.org/?q=*&lang=pt&count={count}&from={from_count}&output=site&sort=&format=summary&fb=&page={page}&filter%5Bin%5D%5B%5D=scl&filter%5Bla%5D%5B%5D=pt&filter%5Bsubject_area%5D%5B%5D=Exact+and+Earth+Sciences&filter%5Btype%5D%5B%5D=research-article"
+
 
 """
 Função para carregamento do conteúdo das URLs de busca na plataforma SciELO
@@ -37,15 +48,11 @@ Função para carregamento do conteúdo das URLs de busca na plataforma SciELO
 :param count: Quantidade de itens carregados
 :return: retorna um objeto contendo os dados da resposta da requisição
 """
-def load_site(page=0, count=50):
+def load_site(url):
     headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36', }
+    response = requests.get(url, headers=headers)
 
-    from_count = 0 if page == 1 else (page * count) - 49
-    link = f"https://search.scielo.org/?q=*&lang=pt&count={count}&from={from_count}&output=site&sort=&format=summary&fb=&page={page}&filter%5Bin%5D%5B%5D=scl&filter%5Bla%5D%5B%5D=pt&filter%5Bsubject_area%5D%5B%5D=Exact+and+Earth+Sciences&filter%5Btype%5D%5B%5D=research-article"
-
-    response = requests.get(link, headers=headers)
-
-    print(f'URL requisitada: {link}')
+    print(f'URL requisitada: {url}')
 
     if response.status_code == 200 :
         print(f"Sucesso: código de retorno {response.status_code}")
@@ -55,11 +62,24 @@ def load_site(page=0, count=50):
 
 
 """
-ParsePage
+ParsePagePaper REESCREVER
 """
-def parsePage(response, df):
+def parsePageIndex(response, df):
     page = BeautifulSoup(response.text, 'html.parser')
-    #results = page.find("div", class_="results")
+    items = page.select("div.results > div.item")
+    
+    for item in items :
+        title = item.select("div.line strong.title")
+        url = item.select("div.line a")
+
+        df = df._append({ "Title" : title[0].text.strip(), "URL" : url[0]['href']}, ignore_index = True)
+    return df
+
+"""
+ParsePageIndex
+"""
+def parsePageIndex(response, df):
+    page = BeautifulSoup(response.text, 'html.parser')
     items = page.select("div.results > div.item")
     
     for item in items :
@@ -70,8 +90,6 @@ def parsePage(response, df):
     return df
 
 
-
-
 """
 Bootstrap section
 """
@@ -80,8 +98,9 @@ df = pd.DataFrame({ "Title" : [], "URL" : [] })
 print(df)
 
 for page in range(1, pages_total+1):
-    res = load_site(page, pages_itens)
-    df = parsePage(res, df)
+    url = parse_url_scielo(page, pages_items)
+    res = load_site(url)
+    df = parsePageIndex(res, df)
 
 
 df.to_excel("Artigos_SciELO.xlsx")
