9b331d589a95a19a8fe8af525bb969234e164c03
Luiz Albano <junior@idesk.com.br>
2024-11-09 11:01:40 -0300

    Importação de dados: função para conexão com arquivo de dados SQLite

--- projeto-final-albano/webcrawler-scielo.py
+++ projeto-final-albano/webcrawler-scielo.py
@@ -2,6 +2,32 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import pandas as pd
+import sqlite3
+
+"""
+Config vars
+"""
+
+# Amount of pages
+pages_total = 130
+
+# Amount of items (papers)
+pages_items = 50
+
+# Database connection
+conn = None
+
+"""
+Open a connection to a SQLite database file.
+"""
+def database_connect(database_file):
+    global conn
+    try:
+        with sqlite3.connect(database_file) as conn:
+            print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+    except sqlite3.OperationalError as err:
+        print("Failed to open datanase: ", database_file, e)
+
 
 
 """
@@ -28,6 +54,9 @@ def load_site(page=0, count=50):
         print(f'Falha na requisição - código do erro: {response.status_code}')
 
 
+"""
+ParsePage
+"""
 def parsePage(response, df):
     page = BeautifulSoup(response.text, 'html.parser')
     #results = page.find("div", class_="results")
@@ -41,8 +70,11 @@ def parsePage(response, df):
     return df
 
 
-pages_total = 130
-pages_itens = 50
+
+
+"""
+Bootstrap section
+"""
 
 df = pd.DataFrame({ "Title" : [], "URL" : [] })
 print(df)
@@ -53,3 +85,12 @@ for page in range(1, pages_total+1):
 
 
 df.to_excel("Artigos_SciELO.xlsx")
+
+
+
+#Colunas: ID, Título, Subtítulo, Palavras-chave, Autores, Resumo, Texto, Ano Publicação, Editors, DOI
+
+# SciELO pages:
+# https://www.scielo.br/j/qn/a/XnhMZ3mhrQWRMTbZJRhqtTF/?lang=pt#
+# https://search.scielo.org/?q=*&lang=pt&count=50&from=1&output=site&sort=&format=summary&fb=&page=1&filter%5Bin%5D%5B%5D=scl&filter%5Bla%5D%5B%5D=pt&filter%5Bsubject_area%5D%5B%5D=Exact+and+Earth+Sciences&filter%5Btype%5D%5B%5D=research-article
+# https://theleftjoin.com/how-to-write-a-pandas-dataframe-to-an-sqlite-table/
